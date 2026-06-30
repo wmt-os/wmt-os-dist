cfc45003f3826afda35a8e128cff88510ed96856
Logan Russell <me@lrussell.net>
2026-06-30 04:19:12 -0400

    Add imlib2 armel package build
    Add imlib2/build-deb.sh, which rebuilds Debian's imlib2 for armel with
    --enable-packing, along with a README documenting why.
    
    Signed-off-by: Logan Russell <me@lrussell.net>

---
+++ imlib2/build-deb.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# Build the imlib2 armel deb packages
+#
+# Output: ./*.deb (override: OUT=)
+# Build-depends: mmdebstrap qemu-user-binfmt
+#
+# Copyright (C) 2026 Logan Russell <me@lrussell.net>
+
+set -eu
+
+SRC=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd)
+OUT=${OUT:-$SRC}
+. "$SRC"/../config.sh
+
+WMTOS_REV=1
+DCH_MSG="Fix PNG unaligned access SIGSEGV on armel by building with --enable-packing."
+
+mkdir -p "$OUT"
+mmdebstrap --variant=buildd --architectures=armel --include="devscripts" \
+	--chrooted-customize-hook="apt-get source imlib2 && cd imlib2-* &&
+		sed -i 's/--enable-rtld-local-support/& --enable-packing/' debian/rules &&
+		export DEBFULLNAME=\"$BUILDER_NAME\" DEBEMAIL=\"$BUILDER_EMAIL\" &&
+		dch -v \"\$(dpkg-parsechangelog -S Version)+wmtos$WMTOS_REV\" -D trixie \"$DCH_MSG\" &&
+		apt-get -y --no-install-recommends build-dep ./ && dpkg-buildpackage -b -uc -us &&
+		mkdir /out && mv /*.deb /out/" \
+	--customize-hook="sync-out /out $OUT" \
+	trixie /dev/null \
+	"https://deb.debian.org/debian" "deb-src https://deb.debian.org/debian trixie main"
+
+rm -f "$OUT"/*-dbgsym*.deb
+ls -1 "$OUT"/*.deb
